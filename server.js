c6aac4d6da55af1a0e1384cbf8cb02a9da32c3a3
Shreyak Jain <shreyakjain14@gmail.com>
2022-01-02 20:22:32 +0530

    heruko deploy error fix

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+const path = require("path");
+const cors = require("cors");
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app>"));
+app.use(cors());
+app.use(express.static("./dist/client-app"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+  res.sendFile(path.join(__dirname, "./../dist/client-app/index.html"));
+  console.log(
+    "path: ",
+    path.join(__dirname, "./../dist/client-app/index.html")
+  );
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
